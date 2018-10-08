b73000a85e0de9a1cd0ee105b5012ea1366771fe
DanielRLeal <daniel.romao.leal.g@gmail.com>
2018-10-08 23:46:11 +0100

    Attack, ok!
    Já ataca
    Tem barra de vida
    
    Do next:
    -> Adicionar "Blend Tree" na animation + ataques;
    ->Adicionar bara de vida do player
    -> (Collide ok!)
    -> ...

--- MyGame/Assets/Scripts/Controllers/EnemyController.cs
+++ MyGame/Assets/Scripts/Controllers/EnemyController.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour {
 
     public float lookRadius = 10f;
     CharacterCombat combat;
     static Animator anim;
-
+    detectHit hitted;
     Transform target;
     NavMeshAgent agent;
+    public Slider healthBar;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
@@ -24,11 +26,19 @@ public class EnemyController : MonoBehaviour {
 	void Update () {
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if( distance <= lookRadius)
+        if (healthBar.value <= 0)
+        {
+            return;
+        }
+
+
+        if ( distance <= lookRadius)
         {
             agent.SetDestination(target.position);
-            anim.SetBool("isRunning", true);
-            anim.SetBool("isAttacking", false);
+            agent.stoppingDistance = 1.75F;
+            //agent.transform.Translate(0,0,0.05F);
+        
+    
 
             if (distance < agent.stoppingDistance)
             {
@@ -36,14 +46,36 @@ public class EnemyController : MonoBehaviour {
                 if(targetStats != null)
                 {
                 combat.Attack(targetStats);
-                    anim.SetBool("isRunning", false);
-                    anim.SetBool("isAttacking", true);
+                    
                 }
+
+                //o inimigo está a ser atacado animation
+                //if (hitted.collider.Equals(gameObject)) { 
+                //    anim.SetBool("isTakingDamage", true);
+                //    anim.SetBool("isRunning", false);
+                //    anim.SetBool("isAttacking", false);
+                //    anim.SetBool("isDead", false);
+
+                //}
                 FaceTarget();
-                
+
             }
         }
-	}
+     
+
+    }
+
+
+    //É colidido pelo Player e perde vida
+    private void OnTriggerEnter(Collider other)
+    {       
+        Debug.Log("Hit");
+        healthBar.value -= 20;
+        if (healthBar.value <= 0)
+        {
+            anim.SetBool("isDead", true);
+        }
+    }
 
     void FaceTarget()
     {
---
+++ MyGame/Assets/Scripts/detectHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class detectHit : MonoBehaviour {
+
+    //public Slider healthBar;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+      
+    //    Debug.Log("Hit");
+    //    healthBar.value -= 20;
+    //        if(healthBar <= 0)
+    //        {
+    //            anim.SetBool("isDead", true);
+    //        }
+        
+    //}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
